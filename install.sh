27ff1d046e5003b0827c88d3e05c6263c6b467c8
rootTHC <root@thc.org>
2020-09-23 06:23:03 +0100

    release tarball

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #! /bin/sh
 
+command -v git >/dev/null 2>&1 || { echo >&2 "git not found. Try 'apt-get install git'"; exit 1; }
 git clone https://github.com/hackerschoice/gsocket.git
 cd gsocket
 ./bootstrap
