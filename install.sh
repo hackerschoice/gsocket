9e51211d2e25bbff428b7fc3bed08074539b1ec1
rootTHC <root@thc.org>
2020-10-26 20:21:24 +0000

    depth 1

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 command -v git >/dev/null 2>&1 || { echo >&2 "git not found. Try 'apt-get install git'"; exit 1; }
-git clone https://github.com/hackerschoice/gsocket.git
+git clone --depth 1 https://github.com/hackerschoice/gsocket.git
 cd gsocket
 ./bootstrap
 ./configure && make && echo "Type 'sudo make install' to install."
