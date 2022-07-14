2034ba58826e9baf0c30237c9c675466170b83ff
SkyperTHC <skyper@thc.org>
2022-07-14 06:44:10 +0100

    osx

--- tools/gsocket_uchroot_dso.c
+++ tools/gsocket_uchroot_dso.c
@@ -113,6 +113,12 @@ thc_init_cyg(void)
 static void thc_init_cyg(void) {}	// Do nothing.
 #endif
 
+#ifdef __APPLE__
+# define DYLD_INTERPOSE(_replacment,_replacee) \
+__attribute__((used)) static struct{ const void* replacment; const void* replacee; } _interpose_##_replacee \
+__attribute__ ((section ("__DATA,__interpose"))) = { (const void*)(unsigned long)&_replacment, (const void*)(unsigned long)&_replacee };
+#endif
+
 static void
 thc_init(void)
 {
