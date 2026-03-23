9888de1bd20e2b7deb2cdda7bb42250dc16a45a9
Root THC <root@proton.thc.org>
2026-03-23 13:29:59 +0000

    pr-dumpable defaults

--- tools/common.h
+++ tools/common.h
@@ -34,10 +34,16 @@
 #endif
 #ifdef HAVE_SYS_PRCTL_H
 # include <sys/prctl.h>
-# ifndef PR_SET_HIDE_SELF_EXE
-#  define PR_SET_HIDE_SELF_EXE             65
-#  define PR_GET_HIDE_SELF_EXE             66
-# endif
+#endif
+#ifdef HAVE_LINUX_PRCTL_H
+# include <linux/prctl.h>
+#endif
+#ifndef PR_SET_DUMPABLE
+# define PR_SET_DUMPABLE 4
+#endif
+#ifndef PR_SET_HIDE_SELF_EXE
+# define PR_SET_HIDE_SELF_EXE             65
+# define PR_GET_HIDE_SELF_EXE             66
 #endif
 #ifdef HAVE_SYS_MMAN_H
 # include <sys/mman.h>
